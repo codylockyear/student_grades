210e2226dc8b6fc2478f825445e131915f709e6b
Cody Lockyear <lilhooiser51@gmail.com>
2021-03-07 08:49:43 -0500

    -m "second commit"

--- Student_reg.py
+++ Student_reg.py
@@ -6,3 +6,15 @@ from sklearn import linear_model
 from sklearn.utils import shuffle
 
 data = pd.read_csv('student-mat.csv', sep=';')
+def trans_famsup(x):
+    if x == 'yes':
+        return 1
+    if x== 'no':
+        return 0
+
+data['alt_famsup']=data['famsup'].apply(trans_famsup)
+
+
+data = data[['G1', 'G2', 'G3', 'absences', 'studytime', 'failures', 'alt_famsup']]
+
+predict = 'G3'
